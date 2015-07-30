ac06ee1e4a8d11a40530ac1bc8ab6b4f0a50450b
Logan Garrett <logan@Logans-MacBook-Pro.local>
2015-07-29 21:12:12 -0400

    json

--- main.rb
+++ main.rb
@@ -10,26 +10,32 @@ $lDoor = Garage::Door.new(23, 17)
 $rDoor = Garage::Door.new(24, 21)
 
 post '/left/open' do
-  $lDoor.open
+  content_type :json
+  { :result => "#{$lDoor.open}" }.to_json
 end
 
 post '/right/open' do
-  $rDoor.open
+  content_type :json
+  { :result => "#{$rDoor.open}" }.to_json
 end
 
 post '/left/close' do
-  $lDoor.close
+  content_type :json
+  { :result => "#{$lDoor.close}" }.to_json
 end
 
 post '/right/close' do
-  $rDoor.close
+  content_type :json
+  { :result => "#{$rDoor.close}" }.to_json
 end
 
+# 0 = closed, 1 = opened
 get '/left/state' do
   content_type :json
   { :state => "#{$lDoor.opened}" }.to_json
 end
 
+# 0 = closed, 1 = opened
 get '/right/state' do
   content_type :json
   { :state => "#{$lDoor.opened}" }.to_json
